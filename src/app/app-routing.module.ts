5e8bb3c72da69123d7ff450e1ec73d9d9680504a
Ahmed khan <ahmed_khan019@hotmail.com>
2023-10-30 21:35:36 +0500

    Added lazy loaded routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  children: [
+    { path: '', loadComponent: () => import('./components/client-list/client-list.component').then(c => c.ClientListComponent)},
+    { path:  `${ROUTES_NAME.CLIENT}/:id`, loadComponent: () => import('./components/invoice-list/invoice-list.component').then(c => c.InvoiceListComponent)},
+  ],
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
