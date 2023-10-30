2a6cf9e275f658075b559d5b467541a9fa3a6f77
Ahmed khan <ahmed_khan019@hotmail.com>
2023-10-30 21:33:20 +0500

    Imported browserAnimation Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,8 +9,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    // Angular
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    // App
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
